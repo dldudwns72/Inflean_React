45022fc5d677450bcccae2c67eeae533aca8fae5
LeeYoungJun <dldudwns72@gmail.com>
2021-04-24 12:12:35 +0900

    keyboard 세팅 및  hostkey 사용

---
+++ game2048/src/util/keyboard.js
@@ -0,0 +1,21 @@
+import hotkeys from "hotkeys-js";
+
+const observerMap = {};
+
+export function addKeyObserver(key,callback){
+    if(!observerMap[key]){
+        observerMap[key] = [];
+        hotkeys(key,() => {excuteCallbacks(key)});
+    }
+    observerMap[key.pysh(callback)]
+}
+
+export function removeKeyObserver(key,callback){
+    observerMap[key] = observerMap[key].filter(item => item !== callback);
+}
+
+function excuteCallbacks(key){
+    for(const ob of observerMap[key]){
+        ob();
+    }
+}
